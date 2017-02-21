becdc3c8854298146496c341653e83a7ffc9a3c3
Felipe Rios <feliperios@Felipes-MacBook-Pro.local>
2017-02-21 18:57:22 -0300

    some fixes

--- Pets/urls.py
+++ Pets/urls.py
@@ -4,4 +4,5 @@ from Pets import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^publish/$', views.publish, name='index'),
+    url(r'^vote/(?P<pet_id>[\w\-]+)/$', views.vote, name='posts'),
 ]
--- Pets/views.py
+++ Pets/views.py
@@ -16,9 +16,11 @@ def publish(request):
         if owner_form.is_valid() and pet_form.is_valid():
             owner = owner_form.save()
             pet = pet_form.save(commit=False)
+            if 'picture' in request.FILES:
+                pet.picture = request.FILES['picture']
             pet.owner = owner
             pet.save()
-            return HttpResponseRedirect('')
+            return HttpResponseRedirect('/')
         else:
             print owner_form.errors, pet_form.errors
     else:
@@ -27,4 +29,12 @@ def publish(request):
     return render(request, 'publish.html', {'owner_form': owner_form, 'pet_form': pet_form})
 
 
-
+def vote(request, pet_id):
+    data = Pet.objects.filter(id=pet_id)
+    if data.exists():
+        pet = Pet.objects.get(id=pet_id)
+        pet.votes += 1
+        pet.save()
+        return HttpResponseRedirect('/')
+    else:
+        return HttpResponseRedirect('/')
